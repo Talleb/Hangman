f3f76442447b75b65ebc9eab0e32bc874e96854e
DimDimDev <dimitris.adabugday@gmail.com>
2020-10-23 19:36:33 +0200

    #17 Random background images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MainPage from './components/MainPage.jsx'
 
 function App() {
+  const BackGrounds = ['Graveyard', 'House', 'Pumpkins', 'Woods', 'Bats']
+  const BackGround = BackGrounds[Math.floor(Math.random() * Math.floor(5))]
+  const [randomBackground, setRandomBackground] = useState(null);
+
+
+  useEffect(() => {
+    function RandomBG() {
+      setRandomBackground(BackGround)
+    }
+    RandomBG();
+  }, [BackGround])
+
   return (
-    <div className="App">
-      <MainPage />
+    <div>{randomBackground ?
+      <main className={randomBackground}>
+        <MainPage />
+      </main> : "loading"}
     </div>
   );
 }
