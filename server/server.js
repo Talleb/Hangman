9d8ff1f9fdd94be1815d512482a04197d75d30dd
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-10-26 17:48:39 +0100

    First Commit #14

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 const randomWords = require('random-words');
-
-
 const word = randomWords();
 
-app.get('/', (req,res) => {
-  res.send({ word: randomWords() }) 
-  // res.send({ word }) -> word does not change on page refresh
-})
+
+app.use( express.static(__dirname + '/../build'));
 
 
 const port = process.env.PORT || 8000;
@@ -17,3 +13,8 @@ app.listen(port, () => {
   console.log('Hangman server listening on port ', port);
   console.log(word);
 })
+
+
+
+// res.send({ word: randomWords() }) 
+// res.send({ word }) -> word does not change on page refresh
