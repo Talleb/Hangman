6836cb68bd7b63b9da3bb22fcb6382c86ae1cd28
Talleb <taulant_09_@hotmail.com>
2020-11-02 10:03:59 +0100

    #21 added landing/loading-page and react-router-dom

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,8 +4,11 @@ import './LandingPage.css'
 const LandingPage = () => {
     return (
         <div>
-            <h1>Welcome to the</h1>
+            <h1>Welcome to the Scary</h1>
             <h1>Spooky Hangman</h1>
+            <h1>Game</h1>
+
+
                 <form>
                 <label>   Name:<input type="text" name="name" />
             </label>
